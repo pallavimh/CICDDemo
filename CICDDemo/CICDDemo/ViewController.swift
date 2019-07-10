41d6f624fc4e1f2d79a90b8a5a88519e0833ba3f
Pallavi Mhaske <pallavi.mhaske@silicus.com>
2019-07-10 14:05:26 +0530

    - Initial project setup.

---
+++ CICDDemo/CICDDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDDemo
+//
+//  Created by Pallavi Mhaske on 10/07/19.
+//  Copyright © 2019 Pallavi Mhaske. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
